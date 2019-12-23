5ba1b93de7ed383246afa98e49d36da9092481d2
yosuheee <yosuheee@gmail.com>
2019-12-23 17:13:03 +0900

    delete comment

--- golf/calculate_core.js
+++ golf/calculate_core.js
@@ -2,32 +2,6 @@ import { Vec3,
          intersection_of_plane_and_line,
          triangle_contains_point } from "../lib/geometry.js";
 
-// 作りたい関数
-// 現在の速度と位置を渡せば、次の速度と位置、それに衝突したかどうかを返してくれる関数
-// これはただの関数でいい（ジェネレータ関数である必要がない）
-//
-// 引数は、
-//   stage: ステージ情報（三角形の情報を取得するのに必要）
-//   qtree: QuadTree情報（候補の三角形のインデックスを高速に取得するために必要）
-//   v: 現在の速度 [m/s]
-//   h: 現在の位置 [m]
-//   t: 何秒後の情報を取得するか [s]
-//   g: 重力加速度 [m/s^2]
-//   F: 風力加速度 [m/s^2]
-//   k: ボールの質量を含めた空気抵抗係数 [1/s]
-//   r: ボールの半径
-//   along_the_ground: 地面に沿っているか
-//
-// 戻り値は、
-//   tout: 実際に何秒経過したか
-//   vout: tout 秒後の速度
-//   hout: tout 秒後の位置
-//   collision: 衝突したかどうか
-//
-// そしたら作り始めよう
-// 思ったよりも簡単に作れそうだ
-// 名前は何にしよう
-// next_position でいいか
 function next_position(stage, qtree, points, v, h, t, g, F, k, r, m, along_the_ground) {
   let vout;
   let hout = h.add(v.scale(t));
