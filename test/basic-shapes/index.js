76b7f7fafa7c773732dc8e549b408f644d2281f5
yosuheee <yosuheee@gmail.com>
2019-12-28 07:42:06 +0900

    change sphere algorithm

--- test/basic-shapes/index.js
+++ test/basic-shapes/index.js
@@ -1,5 +1,5 @@
 import { program, VERTEX_SOURCE, FRAGMENT_SOURCE } from "../../lib/webgl.js";
-import { cube, quarter_cylinder_rect, one_eighth_sphere, rounded_corners_cube } from "../../lib/polygon.js";
+import { cube, quarter_cylinder_rect, one_eighth_sphere, rounded_corners_cube, sphere } from "../../lib/polygon.js";
 import { V3 } from "../../lib/geometry.js";
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -80,6 +80,27 @@ window.addEventListener("DOMContentLoaded", () => {
     gl.clear(gl.COLOR_BUFFER_BIT);
     mo.rotate(V3(1, 1, 1), count)
       .draw(gl, prg, V3(1, 0.5, 1));
+  };
+  tick();
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const gl = document.getElementById("canvas5").getContext("webgl");
+  const prg = program(gl, VERTEX_SOURCE, FRAGMENT_SOURCE);
+
+  gl.enable(gl.DEPTH_TEST);
+
+  const mo = sphere(0.6).model(gl);
+  let count = 0;
+  const tick = () => {
+    requestAnimationFrame(tick);
+    count++;
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    mo.rotate(V3(1, 1, 1), count)
+      .lookAt(V3(0, 0, 2.4), V3(0, 0, 0), V3(0, 1, 0))
+      .perspective(45, 1.0, 0.1, 100)
+      .draw(gl, prg, V3(1, 1, 1));
   };
   tick();
 });
