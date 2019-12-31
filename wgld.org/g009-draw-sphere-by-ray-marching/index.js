1ce46eb74141d1502ce43d48d49c779acacc315d
yosuheee <yosuheee@gmail.com>
2020-01-01 05:57:11 +0900

    add wgld.org draw sphere by ray marching

---
+++ wgld.org/g009-draw-sphere-by-ray-marching/index.js
@@ -0,0 +1,48 @@
+import { program, uniform } from "/module/webgl.js";
+import { rect } from "/module/triangles.js";
+import { fetch_text } from "../../module/util.js";
+
+const vertex_source = `
+attribute vec3 position;
+void main(void) {
+  gl_Position = vec4(position, 1.0);
+}
+`;
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const cvs = document.getElementById("canvas");
+  const gl = cvs.getContext("webgl");
+  const prg = program(gl, vertex_source, await fetch_text("./source.frag"));
+  const mo = rect(2.0, 2.0).translate(-1.0, -1.0, 0).model(gl);
+  
+  const start = new Date().getTime();
+
+  let is_down = false;
+  let mx = 0.5, my = 0.5;
+
+  const change = e => {
+    mx = e.offsetX / gl.canvas.width;
+    my = e.offsetY / gl.canvas.height;
+  };
+
+  cvs.addEventListener("mousedown", e => { is_down = true; change(e); });
+  cvs.addEventListener("mouseup", () => is_down = false);
+  cvs.addEventListener("mousemove", e => { if (is_down) change(e); });
+
+  const render = () => {
+    requestAnimationFrame(render);
+
+    const time = (new Date().getTime() - start) * 0.001;
+
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    uniform(gl, prg, "float", "time", time);
+    uniform(gl, prg, "vec2", "mouse", [mx, my]);
+    uniform(gl, prg, "vec2", "resolution", [gl.canvas.width, gl.canvas.height]);
+
+    mo.draw(gl, prg, { col_name: "", nor_name: "" });
+  };
+
+  render();
+});
---
+++ wgld.org/g009-draw-sphere-by-ray-marching/source.frag
@@ -0,0 +1,38 @@
+precision mediump float;
+
+uniform float time;
+uniform vec2 mouse;
+uniform vec2 resolution;
+
+const float sphereSize = 1.0;
+
+float distanceFunc(vec3 p) {
+  return length(p) - sphereSize;
+}
+
+void main(void) {
+  vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
+
+  vec3 cPos = vec3(0.0,  0.0,  2.0);
+  vec3 cDir = vec3(0.0,  0.0, -1.0);
+  vec3 cUp  = vec3(0.0,  1.0,  0.0);
+  vec3 cSide = cross(cDir, cUp);
+  float targetDepth = 1.0;
+
+  vec3 ray = normalize(cSide * p.x + cUp * p.y + cDir * targetDepth);
+
+  float distance;
+  float rLen = 0.0;
+  vec3  rPos = cPos;
+  for (int i = 0; i < 16; i++) {
+    distance = distanceFunc(rPos);
+    rLen += distance;
+    rPos = cPos + ray * rLen;
+  }
+
+  if (abs(distance) < 0.001) {
+    gl_FragColor = vec4(vec3(1.0), 1.0);
+  } else {
+    gl_FragColor = vec4(vec3(0.0), 1.0);
+  }
+}
