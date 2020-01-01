c1e668457459b0021a33ccd260d94288f4734db5
yosuheee <yosuheee@gmail.com>
2020-01-02 07:52:41 +0900

    update cpp-parser

--- study/haskell/cpp-parser/src/Lib.hs
+++ study/haskell/cpp-parser/src/Lib.hs
@@ -1,1 +1,32 @@
 module Lib where
+
+import Text.Parsec
+import Text.Parsec.String
+
+p_sharp :: Parser Char
+p_sharp = char '#'
+
+p_include_string :: Parser String
+p_include_string = string "include"
+
+p_lt :: Parser Char
+p_lt = char '<'
+
+p_gt :: Parser Char
+p_gt = char '>'
+
+p_not_gt_or_blank :: Parser Char
+p_not_gt_or_blank = noneOf "> "
+
+p_include :: Parser String
+p_include = do
+  p_sharp
+  spaces
+  str <- p_include_string
+  spaces
+  p_lt
+  spaces
+  file <- many1 p_not_gt_or_blank
+  spaces
+  p_gt
+  return file
--- study/haskell/cpp-parser/test/LibSpec.hs
+++ study/haskell/cpp-parser/test/LibSpec.hs
@@ -1,10 +1,43 @@
 module LibSpec (spec) where
 
 import Test.Hspec
+import Text.Parsec
+import Text.Parsec.String
+
 import Lib
 
 spec :: Spec
 spec = do
-  describe "" $ do
-    it "" $ do
-      1 `shouldBe` 1
+  describe "p_sharp" $ do
+    it "accept '#'" $
+      exec p_sharp "#" `shouldBe` "'#'"
+
+  describe "p_include_string" $ do
+    it "accept \"include\"" $
+      exec p_include_string "include" `shouldBe` "\"include\""
+
+  describe "p_lt and p_gt" $ do
+    it "accept '<'" $
+      exec p_lt "<" `shouldBe` "'<'"
+    it "accept '>'" $
+      exec p_gt ">" `shouldBe` "'>'"
+
+  describe "p_not_gt_or_blank" $ do
+    it "reject '>'" $
+      exec p_not_gt_or_blank ">" `shouldNotBe` "'>'"
+    it "reject ' '" $
+      exec p_not_gt_or_blank " " `shouldNotBe` "' '"
+    it "accept 'a'" $
+      exec p_not_gt_or_blank "a" `shouldBe` "'a'"
+
+  describe "p_include" $ do
+    it "accept \"#include<iostream>\"" $
+      exec p_include "#include<iostream>" `shouldBe` "\"iostream\""
+    it "accept \"# include < iostream >\"" $
+      exec p_include "# include < iostream >" `shouldBe` "\"iostream\""
+
+exec :: Show a => Parser a -> String -> String
+exec p input =
+  case parse p "" input of
+    Left  err -> show err
+    Right val -> show val
