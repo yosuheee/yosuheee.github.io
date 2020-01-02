0d13f0c50a24cf79c2e3c59594096d133be0aa26
yosuheee <yosuheee@gmail.com>
2020-01-03 05:23:03 +0900

    add complex expression test

--- study/haskell/cpp-parser/test/ExpressionSpec.hs
+++ study/haskell/cpp-parser/test/ExpressionSpec.hs
@@ -149,6 +149,64 @@ spec = do
       exec p_throw "throw 1" `shouldBe`
         (show $ ExThrow (ExInteger 1))
 
+  describe "some operator" $ do
+    it "accept some operator" $ do
+      let input = "1 * 2 + 3 << 4 <=> 5 <= 6 == 7 & 8 ^ 9 | 10 && 11 || 12 = 13"
+      exec p_priority_16 input `shouldBe` (show $
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+        ExList (
+          ExInteger 1
+          , [("*"  , ExInteger  2)])
+          , [("+"  , ExInteger  3)])
+          , [("<<" , ExInteger  4)])
+          , [("<=>", ExInteger  5)])
+          , [("<=" , ExInteger  6)])
+          , [("==" , ExInteger  7)])
+          , [("&"  , ExInteger  8)])
+          , [("^"  , ExInteger  9)])
+          , [("|"  , ExInteger 10)])
+          , [("&&" , ExInteger 11)])
+          , [("||" , ExInteger 12)])
+          , [("="  , ExInteger 13)]))
+
+    it "accept some operator" $ do
+      let input = "1 = 2 || 3 && 4 | 5 ^ 6 & 7 == 8 <= 9 <=> 10 << 11 + 12 * 13"
+      exec p_priority_16 input `shouldBe` (show $
+        ExList (ExInteger  1, [("="  , 
+        ExList (ExInteger  2, [("||" , 
+        ExList (ExInteger  3, [("&&" , 
+        ExList (ExInteger  4, [("|"  , 
+        ExList (ExInteger  5, [("^"  , 
+        ExList (ExInteger  6, [("&"  , 
+        ExList (ExInteger  7, [("==" , 
+        ExList (ExInteger  8, [("<=" , 
+        ExList (ExInteger  9, [("<=>", 
+        ExList (ExInteger 10, [("<<" , 
+        ExList (ExInteger 11, [("+"  , 
+        ExList (ExInteger 12, [("*"  , ExInteger 13)])
+        )])
+        )])
+        )])
+        )])
+        )])
+        )])
+        )])
+        )])
+        )])
+        )])
+        )])
+        )
+
 exec :: Show a => Parser a -> String -> String
 exec p input =
   case parse p "" input of
