2ec89f22dc4bcbac6d0ad69d3ca612c5af0491df
yosuheee <yosuheee@gmail.com>
2020-01-03 21:18:46 +0900

    add int parser

--- study/haskell/cpp-parser/src/Primitive.hs
+++ study/haskell/cpp-parser/src/Primitive.hs
@@ -3,6 +3,70 @@ module Primitive where
 import Text.Parsec
 import Text.Parsec.String
 
+data NumSuffix = SuNone | SuU | SuL | SuLU | SuLL | SuLLU deriving Show
+
+data Number =
+  NuBin String NumSuffix |
+  NuOct String NumSuffix |
+  NuDec String NumSuffix |
+  NuHex String NumSuffix
+  deriving Show
+
+p_num_suffix :: Parser NumSuffix
+p_num_suffix = do
+  let
+    target = [
+      (SuLLU, [ "llu", "ull", "llU", "ulL", "lLu", "uLl", "lLU", "uLL",
+                "Llu", "Ull", "LlU", "UlL", "LLu", "ULl", "LLU", "ULL" ]),
+      (SuLU, [ "lu", "ul", "lU", "uL", "Lu", "Ul", "LU", "UL" ]),
+      (SuLL, [ "ll", "lL", "Ll", "LL" ]),
+      (SuL, [ "l", "L" ]),
+      (SuU, [ "u", "U" ]) ]
+    make :: NumSuffix -> [String] -> Parser NumSuffix
+    make typ lst = try $ do
+      let (x : xs) = map (try . string) lst
+      foldl (<|>) x xs
+      return typ
+  foldr (<|>) (return SuNone) $ 
+    map (\(typ, lst) -> make typ lst) target
+          
+
+p_num :: Parser Number
+p_num = p_bin <|> p_hex <|> p_oct <|> p_dec
+
+p_hex :: Parser Number
+p_hex = try $ do
+  string "0x"
+  head <- oneOf $ "123456789" ++ abc
+  rest <- many $ digit <|> oneOf abc
+  suff <- p_num_suffix
+  return $ NuHex ([head] ++ rest) suff
+  where
+    abc = "abcdefABCDEF"
+
+p_bin :: Parser Number
+p_bin = try $ do
+  string "0b"
+  head <- char '1'
+  rest <- many $ oneOf "01"
+  suff <- p_num_suffix
+  return $ NuBin ([head] ++ rest) suff
+
+p_oct :: Parser Number
+p_oct = try $ do
+  string "0"
+  head <- oneOf "1234567"
+  rest <- many $ oneOf "01234567"
+  suff <- p_num_suffix
+  return $ NuOct ([head] ++ rest) suff
+
+p_dec :: Parser Number
+p_dec = try $ do
+  head <- oneOf "123456789"
+  rest <- many digit
+  suff <- p_num_suffix
+  return $ NuDec ([head] ++ rest) suff
+
 p_identity :: Parser String
 p_identity = do
   fst <- p_identity_char
--- study/haskell/cpp-parser/test/PrimitiveSpec.hs
+++ study/haskell/cpp-parser/test/PrimitiveSpec.hs
@@ -37,6 +37,46 @@ spec = do
       exec p_identity_and_digit_char "A" `shouldBe` (show 'A')
       exec p_identity_and_digit_char "1" `shouldBe` (show '1')
 
+  describe "number" $ do
+    it "p_hex" $ do
+      exec p_hex "0xaf" `shouldBe` (show $ NuHex "af" SuNone)
+      exec p_hex "0x1A" `shouldBe` (show $ NuHex "1A" SuNone)
+      exec p_hex "0x1g" `shouldNotBe` (show $ NuHex "1g" SuNone)
+      exec p_hex "0x" `shouldNotBe` (show $ NuHex "" SuNone)
+    it "p_oct" $ do
+      exec p_oct "010" `shouldBe` (show $ NuOct "10" SuNone)
+      exec p_oct "023" `shouldBe` (show $ NuOct "23" SuNone)
+      exec p_oct "005" `shouldNotBe` (show $ NuOct "05" SuNone)
+    it "p_bin" $ do
+      exec p_bin "0b10" `shouldBe` (show $ NuBin "10" SuNone)
+      exec p_bin "0b1100011010" `shouldBe` (show $ NuBin "1100011010" SuNone)
+      exec p_bin "0b01" `shouldNotBe` (show $ NuBin "01" SuNone)
+      exec p_bin "0b" `shouldNotBe` (show $ NuBin "" SuNone)
+    it "p_dec" $ do
+      exec p_dec "1234" `shouldBe` (show $ NuDec "1234" SuNone)
+      exec p_dec "1000" `shouldBe` (show $ NuDec "1000" SuNone)
+      exec p_dec "01" `shouldNotBe` (show $ NuDec "01" SuNone)
+    it "p_num" $ do
+      exec p_num "0xaf" `shouldBe` (show $ NuHex "af" SuNone)
+      exec p_num "0100" `shouldBe` (show $ NuOct "100" SuNone)
+      exec p_num "0b10" `shouldBe` (show $ NuBin "10" SuNone)
+      exec p_num "1234" `shouldBe` (show $ NuDec "1234" SuNone)
+
+  describe "p_num_suffix" $ do
+    it "llu" $ do
+      exec p_num "10llu" `shouldBe` (show $ NuDec "10" SuLLU)
+      exec p_num "01llu" `shouldBe` (show $ NuOct "1" SuLLU)
+      exec p_num "10uLl" `shouldBe` (show $ NuDec "10" SuLLU)
+    it "ll" $ do
+      exec p_num "10ll" `shouldBe` (show $ NuDec "10" SuLL)
+    it "lu" $ do
+      exec p_num "10lu" `shouldBe` (show $ NuDec "10" SuLU)
+    it "l" $ do
+      exec p_num "10l" `shouldBe` (show $ NuDec "10" SuL)
+    it "u" $ do
+      exec p_num "10u" `shouldBe` (show $ NuDec "10" SuU)
+
+
 exec :: Show a => Parser a -> String -> String
 exec p input =
   case parse p "" input of
