f35bfe7ef88d64d6deaf074d4c6fd18e18e19c7d
yosuheee <yosuheee@gmail.com>
2020-01-04 21:05:37 +0900

    add parser web application

--- study/haskell/cpp-parser/app/Main.hs
+++ study/haskell/cpp-parser/app/Main.hs
@@ -1,13 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Text.Parsec
-import Text.Parsec.String
+import Network.Wai (responseLBS, Application, requestMethod, pathInfo, getRequestBodyChunk)
+import Network.HTTP.Types (status200)
+import Network.Wai.Handler.Warp (run, Port)
+import System.Environment (getEnvironment)
+import Data.List (lookup)
+import Data.Maybe
+import Data.ByteString.Lazy.Internal (packChars)
+import Data.ByteString.Internal (unpackChars)
 
+import Text.Parsec
 import Statement
 
 main :: IO ()
 main = do
-  input <- getLine
-  case parse p_statement "" input of
-    Left err -> putStrLn $ show err
-    Right val -> putStrLn $ show val
+  port <- getPort
+  putStr "start Server: http://localhost:"
+  print port
+  run port helloApp
+
+helloApp :: Application
+helloApp req respond = do
+  body <- getRequestBodyChunk req
+  value <-
+    if requestMethod req == "POST" && pathInfo req == ["parse"] then do
+      let
+        input = unpackChars body
+        result = 
+          case parse p_statement "" input of
+            Left  err -> ""
+            Right val -> show $ val
+      return result
+    else
+      readFile "app/index.html"
+  respond $ responseLBS status200 [] $ packChars value
+
+getPort :: IO Port
+getPort = getEnvironment >>= return . port
+  where
+    port = fromMaybe defaultPort . fmap read . lookup "PORT"
+
+defaultPort :: Port
+defaultPort = 3000
