a9df08efa11a61e81bf6b91d53ba32aa6d3d2ab4
yosuheee <yosuheee@gmail.com>
2020-01-04 15:03:24 +0900

    add simple declarator parser

--- study/haskell/cpp-parser/src/Statement.hs
+++ study/haskell/cpp-parser/src/Statement.hs
@@ -3,23 +3,27 @@ module Statement where
 import Text.Parsec
 import Text.Parsec.String
 
+import Primitive
 import Expression
 
 data Statement =
   StCompound [Statement] |
-  StExpression Expression
+  StExpression Expression |
+  StDeclarator Type [Variable]
   deriving Show
 
 p_statement :: Parser Statement
 p_statement =
   p_statement_compound <|>
-  p_statement_expression
+  p_statement_expression <|>
+  p_statement_declarator
 
 p_statement_expression :: Parser Statement
 p_statement_expression = try $ do
   exp <- p_expression
   spaces
   char ';'
+  spaces
   return . StExpression $ exp
 
 p_statement_compound :: Parser Statement
@@ -28,4 +32,59 @@ p_statement_compound = try $ do
   body <- many $ spaces *> p_statement
   spaces
   char '}'
+  spaces
   return . StCompound $ body
+
+p_statement_declarator :: Parser Statement
+p_statement_declarator = try $ do
+  typ <- p_simple_type
+  many1 space
+  decls <- p_declarators
+  spaces
+  char ';'
+  spaces
+  return $ StDeclarator typ decls
+
+data Variable = 
+  SetVar String Expression |
+  UnsetVar String
+  deriving Show
+
+p_declarators :: Parser [Variable]
+p_declarators = try $ do
+  let decl = p_set_declarator <|> p_unset_declarator
+  decls <- sepBy decl (char ',' >> spaces)
+  return decls
+
+p_set_declarator :: Parser Variable
+p_set_declarator = try $ do
+  name <- p_identity
+  spaces
+  char '='
+  spaces
+  expr <- p_expression
+  return $ SetVar name expr
+
+p_unset_declarator :: Parser Variable
+p_unset_declarator = try $ do
+  name <- p_identity
+  return $ UnsetVar name
+
+data Type = Type String deriving Show
+
+p_simple_type :: Parser Type
+p_simple_type = try $ do
+  typ <- choice $ map string [
+    "signed char", "unsigned char", "char",
+    "char8_t", "char16_t", "char32_t", "wchar_t",
+    "bool",
+    "int", "short", "long",
+    "signed short int", "unsigned short int",
+    "signed long int",  "unsigned long int",
+    "signed int",       "unsigned int",
+    "signed short",     "unsigned short",
+    "signed long",      "unsigned long",
+    "signed",           "unsigned",
+    "long double", "float", "double",
+    "void" ]
+  return . Type $ typ
--- study/haskell/cpp-parser/test/StatementSpec.hs
+++ study/haskell/cpp-parser/test/StatementSpec.hs
@@ -11,6 +11,12 @@ import Statement
 
 spec :: Spec
 spec = do
+  it "statements" $ do
+    exec p_statement "{ int a = 1; a + 1; }" `shouldBe`
+      (show $ StCompound [
+        StDeclarator (Type "int") [SetVar "a" (ExInteger 1)],
+        StExpression (ExBinary "+" (ExIdentity "a") (ExInteger 1)) ])
+
   it "expression;" $ do
     1 `shouldBe` 1
     exec p_statement_expression "1;" `shouldBe` (show $ StExpression (ExInteger 1))
@@ -22,3 +28,18 @@ spec = do
       (show $ StCompound [StCompound [StExpression (ExInteger 1)], StExpression (ExInteger 2), StCompound []])
     exec p_statement_compound "{{1;} 2;{}}" `shouldBe`
       (show $ StCompound [StCompound [StExpression (ExInteger 1)], StExpression (ExInteger 2), StCompound []])
+
+  it "simple type" $ do
+    exec p_simple_type "char" `shouldBe` (show $ Type "char")
+
+  it "simple declarator" $ do
+    exec p_set_declarator "a = 1" `shouldBe` (show $ SetVar "a" (ExInteger 1))
+    exec p_unset_declarator "a" `shouldBe` (show $ UnsetVar "a")
+
+  it "simple declarators" $ do
+    exec p_declarators "a = 1, b, c = 'C'" `shouldBe`
+      (show $ [SetVar "a" (ExInteger 1), UnsetVar "b", SetVar "c" (ExChar 'C')])
+
+  it "simple declarator statement" $ do
+    exec p_statement_declarator "int a = 1, b;" `shouldBe`
+      (show $ StDeclarator (Type "int") [SetVar "a" (ExInteger 1), UnsetVar "b"])
