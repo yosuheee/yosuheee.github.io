a7fa1ed1e0afbfd04f0ebc5efa21215d8d068930
yosuheee <yosuheee@gmail.com>
2020-01-05 19:22:53 +0900

    add attribute parser

--- study/haskell/cpp-parser/src/Attribute.hs
+++ study/haskell/cpp-parser/src/Attribute.hs
@@ -2,3 +2,105 @@ module Attribute where
 
 import Text.Parsec
 import Text.Parsec.String
+
+import Primitive.Identity
+import Primitive.Integer
+
+data AtList =
+  AtUsingList String [Attribute] |
+  AtList [Attribute]
+  deriving (Show)
+
+data Attribute =
+  AtIdentity String |
+  AtNamespace [String] String |
+  AtFunction String AtArgument |
+  AtNamespaceFunction [String] String AtArgument
+  deriving (Show)
+
+data AtArgument =
+  AtArgument [AtPrimitive]
+  deriving (Show)
+
+data AtPrimitive =
+  AtInteger Integer
+  deriving (Show)
+
+p_attributes_block :: Parser AtList
+p_attributes_block = 
+  p_attribute_using_list <|>
+  p_attribute_list
+
+p_attribute_using_list :: Parser AtList
+p_attribute_using_list = try $ do
+  string "[["
+  spaces
+  string "using"
+  skipMany1 space
+  name <- p_identity
+  spaces
+  char ':'
+  spaces
+  attrs <- sepBy (try $ p_attribute <* spaces) (try $ char ',' >> spaces)
+  string "]]"
+  return $ AtUsingList name attrs
+
+p_attribute_list :: Parser AtList
+p_attribute_list = try $ do
+  string "[["
+  spaces
+  attrs <- sepBy (try $ p_attribute <* spaces) (try $ char ',' >> spaces)
+  string "]]"
+  return $ AtList attrs
+
+p_attribute :: Parser Attribute
+p_attribute =
+  p_attribute_namespace_function <|>
+  p_attribute_namespace <|>
+  p_attribute_function <|>
+  p_attribute_identity
+
+p_attribute_primitive :: Parser AtPrimitive
+p_attribute_primitive =
+  (AtInteger <$> p_integer)
+
+p_attribute_identity :: Parser Attribute
+p_attribute_identity = try $ do
+  name <- p_identity
+  spaces
+  return $ AtIdentity name
+
+p_attribute_function :: Parser Attribute
+p_attribute_function = try $ do
+  name <- p_identity
+  spaces
+  char '('
+  spaces
+  args <- p_arguments
+  char ')'
+  spaces
+  return $ AtFunction name args
+
+p_attribute_namespace :: Parser Attribute
+p_attribute_namespace = try $ do
+  prefs <- many1 . try $ p_identity <* (spaces >> string "::" >> spaces)
+  tail <- p_identity
+  spaces
+  return $ AtNamespace prefs tail
+
+p_attribute_namespace_function :: Parser Attribute
+p_attribute_namespace_function = try $ do
+  prefs <- many1 . try $ p_identity <* (spaces >> string "::" >> spaces)
+  tail <- p_identity
+  spaces
+  char '('
+  spaces
+  args <- p_arguments
+  char ')'
+  spaces
+  return $ AtNamespaceFunction prefs tail args
+  
+p_arguments :: Parser AtArgument
+p_arguments = try $ do
+  attrs <- sepBy (try $ p_attribute_primitive <* spaces) (try (char ',' >> spaces))
+  return $ AtArgument attrs
--- study/haskell/cpp-parser/test/AttributeSpec.hs
+++ study/haskell/cpp-parser/test/AttributeSpec.hs
@@ -2,9 +2,66 @@ module AttributeSpec (spec) where
 
 import Test.Hspec
 
+import Util
 import Attribute
 
 spec :: Spec
 spec = do
-  it "a" $ do
-    1 `shouldBe` 1
+  describe "one identifier attribute" $ do
+    it "standard" $ do
+      exec p_attribute_list "[[ noreturn ]]" `shouldBe`
+        (show $ AtList [AtIdentity "noreturn"])
+
+  describe "attribute list" $ do
+    it "standard" $ do
+      exec p_attribute_list "[[ a, b, c ]]" `shouldBe`
+        (show $ AtList [AtIdentity "a", AtIdentity "b", AtIdentity "c"])
+
+  describe "attribute namespace" $ do
+    it "standard" $ do
+      exec p_attribute_namespace "CC::opt" `shouldBe`
+        (show $ AtNamespace ["CC"] "opt")
+    it "combinate" $ do
+      exec p_attributes_block "[[ CC::opt, a ]]" `shouldBe`
+        (show $ AtList [AtNamespace ["CC"] "opt", AtIdentity "a"])
+
+  describe "argument list" $ do
+    it "standard" $ do
+      exec p_arguments "1, 2, 3" `shouldBe`
+        (show $ AtArgument [AtInteger 1, AtInteger 2, AtInteger 3])
+    it "function" $ do
+      exec p_attribute_function "func(1, 2, 3)" `shouldBe`
+        (show $ AtFunction "func" $
+          AtArgument [AtInteger 1, AtInteger 2, AtInteger 3])
+    it "combinate" $ do
+      exec p_attributes_block "[[ CC::opt, a, f(1, 2) ]]" `shouldBe`
+        (show $ AtList [
+          AtNamespace ["CC"] "opt",
+          AtIdentity "a",
+          AtFunction "f" $ AtArgument [AtInteger 1, AtInteger 2]])
+
+  describe "namespace argument" $ do
+    it "standard" $ do
+      exec p_attribute_namespace_function "cats::meow(1)" `shouldBe`
+        (show $ AtNamespaceFunction ["cats"] "meow" $ AtArgument [AtInteger 1])
+    it "combinate" $ do
+      exec p_attributes_block "[[ CC :: opt , a , f ( 1, 2 ) , cats :: meow ( 5 ) ]]" `shouldBe`
+        (show $ AtList [
+          AtNamespace ["CC"] "opt",
+          AtIdentity "a",
+          AtFunction "f" $ AtArgument [AtInteger 1, AtInteger 2],
+          AtNamespaceFunction ["cats"] "meow" $ AtArgument [AtInteger 5] ])
+
+  describe "using" $ do
+    it "standard" $ do
+      exec p_attribute_using_list "[[ using gnu : const , always ]]" `shouldBe`
+        (show $ AtUsingList "gnu" [
+          AtIdentity "const",
+          AtIdentity "always" ])
+    it "combinate" $ do
+      exec p_attributes_block "[[ using s : CC :: opt , a , f ( 1, 2 ) , cats :: meow ( 5 ) ]]" `shouldBe`
+        (show $ AtUsingList "s" [
+          AtNamespace ["CC"] "opt",
+          AtIdentity "a",
+          AtFunction "f" $ AtArgument [AtInteger 1, AtInteger 2],
+          AtNamespaceFunction ["cats"] "meow" $ AtArgument [AtInteger 5] ])
