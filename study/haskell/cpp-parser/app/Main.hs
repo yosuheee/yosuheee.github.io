be58cbc83728ea6ed2a919213eb504a75a3fae93
yosuheee <yosuheee@gmail.com>
2020-01-05 09:39:51 +0900

    add save_code option to web application

--- study/haskell/cpp-parser/app/Main.hs
+++ study/haskell/cpp-parser/app/Main.hs
@@ -9,9 +9,9 @@ import Network.Wai.Handler.Warp (run, Port)
 import System.Environment (getEnvironment)
 import Data.List (lookup)
 import Data.Maybe
-import Data.ByteString.Lazy as DBLI (fromStrict)
-import Data.ByteString.Lazy.Internal as DBL (unpackChars, packChars)
-import Data.ByteString.Internal as DB (unpackChars)
+import Data.ByteString.Internal as DBI (unpackChars)
+import Data.ByteString.Lazy as DBL (fromStrict)
+import Data.ByteString.Lazy.Internal as DBLI (unpackChars, packChars)
 
 import Text.Parsec
 import Data.Aeson as DA (FromJSON, ToJSON, encode, decode)
@@ -37,8 +37,8 @@ helloApp req respond = do
   body <- getRequestBodyChunk req
   value <-
     if requestMethod req == "POST" && pathInfo req == ["parse"] then do
-      let input = DB.unpackChars body
-      let dat = DBLI.fromStrict body
+      let input = DBI.unpackChars body
+      let dat = DBL.fromStrict body
           (Just mydat) = DA.decode dat :: Maybe MyData
           input = source mydat
 
@@ -46,16 +46,16 @@ helloApp req respond = do
         return " "
       else
         return $
-          case parse p_statement "" input of
+          case parse (spaces *> p_statement <* eof) "" input of
             Left err -> show err
             Right val ->
               if kind mydat == "json" then
-                DBL.unpackChars . DA.encode $ val
+                DBLI.unpackChars . DA.encode $ val
               else
                 show val
     else
       readFile "app/index.html"
-  respond $ responseLBS status200 [] $ DBL.packChars value
+  respond $ responseLBS status200 [] $ DBLI.packChars value
 
 getPort :: IO Port
 getPort = getEnvironment >>= return . port
