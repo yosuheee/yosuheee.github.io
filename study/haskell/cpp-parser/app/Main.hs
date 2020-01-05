ac06ff3dd41ae0c502368fd6ae368b86eefb6669
yosuheee <yosuheee@gmail.com>
2020-01-05 09:04:12 +0900

    add options to web application

--- study/haskell/cpp-parser/app/Main.hs
+++ study/haskell/cpp-parser/app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
@@ -8,15 +9,21 @@ import Network.Wai.Handler.Warp (run, Port)
 import System.Environment (getEnvironment)
 import Data.List (lookup)
 import Data.Maybe
-import Data.ByteString.Lazy.Internal as DBL
-import Data.ByteString.Internal as DB
+import Data.ByteString.Lazy as DBLI (fromStrict)
+import Data.ByteString.Lazy.Internal as DBL (unpackChars, packChars)
+import Data.ByteString.Internal as DB (unpackChars)
 
 import Text.Parsec
-import Data.Aeson as DA
+import Data.Aeson as DA (FromJSON, ToJSON, encode, decode)
+import GHC.Generics
 
 import Statement
 
 import Util
+
+data MyData = MyData { kind :: String, source :: String } deriving (Show, Generic)
+instance DA.FromJSON MyData
+instance DA.ToJSON MyData
 
 main :: IO ()
 main = do
@@ -31,13 +38,21 @@ helloApp req respond = do
   value <-
     if requestMethod req == "POST" && pathInfo req == ["parse"] then do
       let input = DB.unpackChars body
+      let dat = DBLI.fromStrict body
+          (Just mydat) = DA.decode dat :: Maybe MyData
+          input = source mydat
+
       if input == "" then
         return " "
       else
         return $
           case parse p_statement "" input of
             Left err -> show err
-            Right val -> DBL.unpackChars . DA.encode $ val
+            Right val ->
+              if kind mydat == "json" then
+                DBL.unpackChars . DA.encode $ val
+              else
+                show val
     else
       readFile "app/index.html"
   respond $ responseLBS status200 [] $ DBL.packChars value
