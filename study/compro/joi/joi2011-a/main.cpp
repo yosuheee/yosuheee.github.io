d819df765c92048212f90b5e54900af426dff5f6
yosuheee <yosuheee@gmail.com>
2020-01-17 18:33:11 +0900

    add joi2011 a and b

---
+++ study/compro/joi/joi2011-a/main.cpp
@@ -0,0 +1,78 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0560
+
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < int(n); i++)
+#define rrep(i, n) for (int i = int(n) - 1; i >= 0; i--)
+#define reps(i, n) for (int i = 1; i <= int(n); i++)
+#define rreps(i, n) for (int i = int(n); i >= 1; i--)
+#define repc(i, n) for (int i = 0; i <= int(n); i++)
+#define rrepc(i, n) for (int i = int(n); i >= 0; i--)
+#define repi(i, a, b) for (int i = int(a); i < int(b); i++)
+#define repic(i, a, b) for (int i = int(a); i <= int(b); i++)
+#define each(x, y) for (auto &x : y)
+#define all(a) (a).begin(), (a).end()
+#define bit(b) (1ll << (b))
+
+using namespace std;
+
+using i32 = int;
+using i64 = long long;
+using u64 = unsigned long long;
+using f80 = long double;
+using vi32 = vector<i32>;
+using vi64 = vector<i64>;
+using vu64 = vector<u64>;
+using vf80 = vector<f80>;
+using vstr = vector<string>;
+
+inline void yes() { cout << "Yes" << '\n'; exit(0); }
+inline void no() { cout << "No" << '\n'; exit(0); }
+inline i64 gcd(i64 a, i64 b) { if (min(a, b) == 0) return max(a, b); if (a % b == 0) return b; return gcd(b, a % b); }
+inline i64 lcm(i64 a, i64 b) { return a / gcd(a, b) * b; }
+inline u64 xorshift() { static u64 x = 88172645463325252ull; x = x ^ (x << 7); return x = x ^ (x >> 9); }
+void solve(); int main() { ios::sync_with_stdio(0); cin.tie(0); cout << fixed << setprecision(16); solve(); return 0; }
+template <typename T> class pqasc : public priority_queue<T, vector<T>, greater<T>> {};
+template <typename T> class pqdesc : public priority_queue<T, vector<T>, less<T>> {};
+template <typename T> inline void amax(T &x, T y) { if (x < y) x = y; }
+template <typename T> inline void amin(T &x, T y) { if (x > y) x = y; }
+template <typename T> inline T power(T x, i64 n, T e = 1) { T r = e; while (n > 0) { if (n & 1) r *= x; x *= x; n >>= 1; } return r; }
+template <typename T> istream& operator>>(istream &is, vector<T> &v) { each(x, v) is >> x; return is; }
+template <typename T> ostream& operator<<(ostream &os, vector<T> &v) { rep(i, v.size()) { if (i) os << ' '; os << v[i]; } return os; }
+template <typename T, typename S> istream& operator>>(istream &is, pair<T, S> &p) { is >> p.first >> p.second; return is; }
+template <typename T, typename S> ostream& operator<<(ostream &os, pair<T, S> &p) { os << p.first << ' ' << p.second; return os; }
+
+void solve() {
+  int n, m;
+  cin >> m >> n;
+  int k;
+  cin >> k;
+  vector<vi32> ja = vector<vi32>(m + 1, vi32(n + 1));
+  vector<vi32> um = vector<vi32>(m + 1, vi32(n + 1));
+  vector<vi32> ko = vector<vi32>(m + 1, vi32(n + 1));
+  reps(i, m) {
+    string s;
+    cin >> s;
+    reps(j, n) {
+      switch (s[j - 1]) {
+        case 'J': ja[i][j] = 1; break;
+        case 'O': um[i][j] = 1; break;
+        case 'I': ko[i][j] = 1; break;
+      }
+    }
+  }
+  reps(i, m) reps(j, n) {
+    ja[i][j] += ja[i-1][j] + ja[i][j-1] - ja[i-1][j-1];
+    um[i][j] += um[i-1][j] + um[i][j-1] - um[i-1][j-1];
+    ko[i][j] += ko[i-1][j] + ko[i][j-1] - ko[i-1][j-1];
+  }
+  rep(_, k) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    a--, b--;
+    vi32 ans = { ja[c][d] - ja[a][d] - ja[c][b] + ja[a][b],
+                 um[c][d] - um[a][d] - um[c][b] + um[a][b],
+                 ko[c][d] - ko[a][d] - ko[c][b] + ko[a][b] };
+    cout << ans << endl;
+  }
+}
